6c58912597a6b9625ed801927351098a9a590662
anassfiqhi3 <anassfiqhi3@gmail.com>
2025-06-22 20:14:40 +0100

    changing the badge color

--- storefront/src/modules/layout/components/cart-dropdown/index.tsx
+++ storefront/src/modules/layout/components/cart-dropdown/index.tsx
@@ -88,7 +88,7 @@ const CartDropdown = ({
             <div className="relative">
               <ShoppingBagIcon className="w-6 h-6 text-white" >
               </ShoppingBagIcon>
-              <Badge className="flex items-center justify-center bg-[#b1b1b1] text-black w-[14px] h-[14px] p-0 rounded-full font-mono tabular-nums absolute top-[-4px] right-[-4px]">
+              <Badge className="flex items-center justify-center bg-[#b1b1b1] text-black text-xs w-[14px] h-[14px] p-0 rounded-full font-mono tabular-nums absolute top-[-4px] right-[-4px]">
                 {totalItems}
               </Badge>
             </div>
