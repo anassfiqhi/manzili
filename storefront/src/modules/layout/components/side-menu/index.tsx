79ce0b34ee889409699ec8b5f349c38ac6db64d1
anassfiqhi3 <anassfiqhi3@gmail.com>
2025-06-27 16:57:30 +0100

    Changing footer store name

--- storefront/src/modules/layout/components/side-menu/index.tsx
+++ storefront/src/modules/layout/components/side-menu/index.tsx
@@ -93,7 +93,7 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                         />
                       </div>
                       <Text className="flex justify-between txt-compact-small">
-                        © {new Date().getFullYear()} Medusa Store. All rights
+                        © {new Date().getFullYear()} Manzili Store. All rights
                         reserved.
                       </Text>
                     </div>
