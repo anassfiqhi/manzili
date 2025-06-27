b646c2802d94a89c3ae95a24686502063d369c89
anassfiqhi3 <anassfiqhi3@gmail.com>
2025-06-27 14:32:14 +0100

    Changing cash on delivery icon in payment section sizes

--- storefront/src/modules/order/components/payment-details/index.tsx
+++ storefront/src/modules/order/components/payment-details/index.tsx
@@ -34,7 +34,7 @@ const PaymentDetails = ({ order }: PaymentDetailsProps) => {
             </div>
             <div className="flex flex-col w-2/3">
               <Text className="txt-medium-plus text-ui-fg-base mb-1">
-                Payment details
+                Payment detailsssss
               </Text>
               <div className="flex gap-2 txt-medium text-ui-fg-subtle items-center">
                 <Container className="flex items-center h-7 w-fit p-2 bg-ui-button-neutral-hover">
