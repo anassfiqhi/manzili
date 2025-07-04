906c7173d20b035fb2df90fc885043f5980abce2
anassfiqhi3 <anassfiqhi3@gmail.com>
2025-07-04 23:26:15 +0100

    Adding text and simple mesurements

--- storefront/src/modules/home/components/hero-carousel/index.tsx
+++ storefront/src/modules/home/components/hero-carousel/index.tsx
@@ -27,35 +27,53 @@ export function HeroCarousel() {
       <CarouselContent className="!ml-0">
         <CarouselItem className="!pl-0 w-full">
           <div className="w-full h-[45vh] md:h-[60vh] lg:h-[75vh] xl:h-[85vh] 2xl:h-[45vh] relative">
-            <Image 
-              src="/banner1.png" 
-              alt="banner 1" 
+            <Image
+              src="/banner1.png"
+              alt="banner 1"
               fill
-              sizes="100vw" 
-              className="object-cover" 
+              sizes="100vw"
+              className="object-cover"
             />
+            <div className="absolute top-0 left-0 right-0 bottom-0 z-10">
+              <div className="mx-auto px-4 py-28">
+                <h2 className="text-5xl text-white"><b>We deliver </b>the ideal</h2>
+                <p className="text-white">Our carefully curated selection of bath products is designed to elevate your bathing ritual to new heights.</p>
+              </div>
+            </div>
           </div>
         </CarouselItem>
         <CarouselItem className="!pl-0 w-full">
           <div className="w-full h-[45vh] md:h-[60vh] lg:h-[75vh] xl:h-[85vh] 2xl:h-[45vh] relative">
-            <Image 
-              src="/banner2.png" 
-              alt="banner 2" 
+            <Image
+              src="/banner2.png"
+              alt="banner 2"
               fill
-              sizes="100vw" 
-              className="object-cover" 
+              sizes="100vw"
+              className="object-cover"
             />
+            <div className="absolute top-0 left-0 right-0 bottom-0 z-10">
+              <div className="mx-auto px-4 py-28">
+                <h2 className="text-5xl text-white"><b>We deliver </b>the ideal</h2>
+                <p className="text-white">Our carefully curated selection of bath products is designed to elevate your bathing ritual to new heights.</p>
+              </div>
+            </div>
           </div>
         </CarouselItem>
         <CarouselItem className="!pl-0 w-full">
           <div className="w-full h-[45vh] md:h-[60vh] lg:h-[75vh] xl:h-[85vh] 2xl:h-[45vh] relative">
-            <Image 
-              src="/banner3.png" 
-              alt="banner 3" 
+            <Image
+              src="/banner3.png"
+              alt="banner 3"
               fill
-              sizes="100vw" 
-              className="object-cover" 
+              sizes="100vw"
+              className="object-cover"
             />
+            <div className="absolute top-0 left-0 right-0 bottom-0 z-10">
+              <div className="mx-auto px-4 py-28">
+                <h2 className="text-5xl text-white"><b>We deliver </b>the ideal</h2>
+                <p className="text-white">Our carefully curated selection of bath products is designed to elevate your bathing ritual to new heights.</p>
+              </div>
+            </div>
           </div>
         </CarouselItem>
       </CarouselContent>
