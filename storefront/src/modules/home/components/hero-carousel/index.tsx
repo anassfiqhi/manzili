7acc0c677b6a9290a200635dca7627d4e3a985b8
anassfiqhi3 <anassfiqhi3@gmail.com>
2025-07-05 18:27:24 +0100

    fixing sizes and fonts of the carousel items

--- storefront/src/modules/home/components/hero-carousel/index.tsx
+++ storefront/src/modules/home/components/hero-carousel/index.tsx
@@ -50,9 +50,9 @@ export function HeroCarousel() {
                     <MoveRight className="ml-2 h-4 w-4 text-black" />
                   </LocalizedClientLink>
                   <LocalizedClientLink
-                    href="/categories"
+                    href="/store"
                     className="flex justify-center items-center text-center px-3 py-4 bg-transparent font-medium text-sm h-9 text-white border border-white border-solid rounded-full"
-                    data-testid="categories-link"
+                    data-testid="store-link"
                   >
                     Show our products
                     <MoveRight className="ml-2 h-4 w-4 text-white" />
@@ -85,9 +85,9 @@ export function HeroCarousel() {
                     <MoveRight className="ml-2 h-4 w-4 text-black" />
                   </LocalizedClientLink>
                   <LocalizedClientLink
-                    href="/categories"
+                    href="/store"
                     className="flex justify-center items-center text-center px-3 py-4 bg-transparent font-medium text-sm h-9 text-white border border-white border-solid rounded-full"
-                    data-testid="categories-link"
+                    data-testid="store-link"
                   >
                     Show our products
                     <MoveRight className="ml-2 h-4 w-4 text-white" />
@@ -120,9 +120,9 @@ export function HeroCarousel() {
                     <MoveRight className="ml-2 h-4 w-4 text-black" />
                   </LocalizedClientLink>
                   <LocalizedClientLink
-                    href="/categories"
+                    href="/store"
                     className="flex justify-center items-center text-center px-3 py-4 bg-transparent font-medium text-sm h-9 text-white border border-white border-solid rounded-full"
-                    data-testid="categories-link"
+                    data-testid="store-link"
                   >
                     Show our products
                     <MoveRight className="ml-2 h-4 w-4 text-white" />
