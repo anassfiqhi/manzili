64b247676be43b51bafb44b845b610a9b6edaafc
anassfiqhi3 <anassfiqhi3@gmail.com>
2025-07-05 23:36:13 +0100

    fixing fonts and sizes

--- storefront/src/modules/home/components/hero-carousel/index.tsx
+++ storefront/src/modules/home/components/hero-carousel/index.tsx
@@ -28,7 +28,7 @@ export function HeroCarousel() {
     <Carousel className="w-full" setApi={setApi} opts={{ loop: true }}>
       <CarouselContent className="!ml-0">
         <CarouselItem className="!pl-0 w-full">
-          <div className="w-full max-[376px]:h-[344px] h-[50vh] md:h-[60vh] lg:h-[75vh] xl:h-[85vh] 2xl:h-[45vh] relative">
+          <div className="w-full h-[50vh] min-[320px]:h-[344px] min-[375px]:h-[344px] min-[425px]:h-[299px] md:h-[498px] lg:h-[498px] xl:h-[721px] 2xl:h-[721px] relative">
             <Image
               src="/banner1.png"
               alt="banner 1"
@@ -37,9 +37,9 @@ export function HeroCarousel() {
               className="object-cover"
             />
             <div className="absolute top-0 left-0 right-0 bottom-0 z-10">
-              <div className="mx-auto px-4 py-10 md:py-24 lg:py-24 xl:py-24 2xl:py-40 md:max-w-md md:ml-0">
-                <h2 className="text-[26px] md:text-4xl lg:text-6xl xl:text-7xl mb-4 md:mb-6 lg:mb-8 text-white font-serif"><b>We deliver </b>the ideal</h2>
-                <p className="text-sm mb-8 text-white" style={{ fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Ubuntu, sans-serif' }}>Our carefully curated selection of bath products is designed to elevate your bathing ritual to new heights.</p>
+              <div className="mx-auto px-4 py-[40px] md:py-[60px] md:max-w-[1440px] md:px-6 md:ml-0">
+                <h2 className="text-[26px] max-[425px]:text-[26px] md:text-[41px] lg:text-[56px] min-[1440px]:text-[70px] mb-4 md:mb-6 lg:mb-8 text-white font-serif"><b>We deliver </b>the ideal</h2>
+                <p className="text-[14px] mb-8 text-white" style={{ fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Ubuntu, sans-serif' }}>Our carefully curated selection of bath products is designed to elevate your bathing ritual to new heights.</p>
                 <div className="flex flex-col min-[376px]:flex-row justify-center items-center gap-3">
                   <LocalizedClientLink
                     href="/categories"
@@ -63,7 +63,7 @@ export function HeroCarousel() {
           </div>
         </CarouselItem>
         <CarouselItem className="!pl-0 w-full">
-          <div className="w-full max-[376px]:h-[344px] h-[50vh] md:h-[60vh] lg:h-[75vh] xl:h-[85vh] 2xl:h-[45vh] relative">
+          <div className="w-full h-[50vh] min-[320px]:h-[344px] min-[375px]:h-[344px] min-[425px]:h-[299px] md:h-[498px] lg:h-[498px] xl:h-[721px] 2xl:h-[721px] relative">
             <Image
               src="/banner2.png"
               alt="banner 2"
@@ -72,9 +72,9 @@ export function HeroCarousel() {
               className="object-cover"
             />
             <div className="absolute top-0 left-0 right-0 bottom-0 z-10">
-              <div className="mx-auto px-4 py-10 md:py-24 lg:py-24 xl:py-24 2xl:py-40 md:max-w-md md:ml-0">
-                <h2 className="text-[26px] md:text-4xl lg:text-6xl xl:text-7xl mb-4 md:mb-6 lg:mb-8 text-white font-serif"><b>Your bathroom </b>vision made possible.</h2>
-                <p className="text-sm mb-8 text-white" style={{ fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Ubuntu, sans-serif' }}>Imagine stepping into your newly renovated bathroom, greeted by the soft glow of carefully placed lighting and the soothing sound of a cascading waterfall shower.</p>
+              <div className="mx-auto px-4 py-[40px] md:py-[60px] md:max-w-[1440px] md:px-6 md:ml-0">
+                <h2 className="text-[26px] max-[425px]:text-[26px] md:text-[41px] lg:text-[56px] min-[1440px]:text-[70px] mb-4 md:mb-6 lg:mb-8 text-white font-serif"><b>Your bathroom </b>vision made possible.</h2>
+                <p className="text-[14px] mb-8 text-white" style={{ fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Ubuntu, sans-serif' }}>Imagine stepping into your newly renovated bathroom, greeted by the soft glow of carefully placed lighting and the soothing sound of a cascading waterfall shower.</p>
                 <div className="flex flex-col min-[376px]:flex-row justify-center items-center gap-3">
                   <LocalizedClientLink
                     href="/categories"
@@ -98,7 +98,7 @@ export function HeroCarousel() {
           </div>
         </CarouselItem>
         <CarouselItem className="!pl-0 w-full">
-          <div className="w-full max-[376px]:h-[344px] h-[50vh] md:h-[60vh] lg:h-[75vh] xl:h-[85vh] 2xl:h-[45vh] relative">
+          <div className="w-full h-[50vh] min-[320px]:h-[344px] min-[375px]:h-[344px] min-[425px]:h-[299px] md:h-[498px] lg:h-[498px] xl:h-[721px] 2xl:h-[721px] relative">
             <Image
               src="/banner3.png"
               alt="banner 3"
@@ -107,9 +107,9 @@ export function HeroCarousel() {
               className="object-cover"
             />
             <div className="absolute top-0 left-0 right-0 bottom-0 z-10">
-              <div className="mx-auto px-4 py-10 md:py-24 lg:py-24 xl:py-24 2xl:py-40 md:max-w-md md:ml-0">
-                <h2 className="text-[26px] md:text-4xl lg:text-6xl xl:text-7xl mb-4 md:mb-6 lg:mb-8 text-white font-serif"><b>Bathroom design </b>showroom</h2>
-                <p className="text-sm mb-8 text-white" style={{ fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Ubuntu, sans-serif' }}>The finest materials combined with traditional manufacturing skills make for the perfect look</p>
+              <div className="mx-auto px-4 py-[40px] md:py-[60px] md:max-w-[1440px] md:px-6 md:ml-0">
+                <h2 className="text-[26px] max-[425px]:text-[26px] md:text-[41px] lg:text-[56px] min-[1440px]:text-[70px] mb-4 md:mb-6 lg:mb-8 text-white font-serif"><b>Bathroom design </b>showroom</h2>
+                <p className="text-[14px] mb-8 text-white" style={{ fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Ubuntu, sans-serif' }}>The finest materials combined with traditional manufacturing skills make for the perfect look</p>
                 <div className="flex flex-col min-[376px]:flex-row justify-center items-center gap-3">
                   <LocalizedClientLink
                     href="/categories"
