517c0399f2f9ba884b6d1f8cc9ec08fc7d774630
anassfiqhi3 <anassfiqhi3@gmail.com>
2025-07-09 21:05:39 +0100

    Translating cart dropdown

--- storefront/src/modules/layout/components/cart-dropdown/index.tsx
+++ storefront/src/modules/layout/components/cart-dropdown/index.tsx
@@ -112,7 +112,7 @@ const CartDropdown = ({
             data-testid="nav-cart-dropdown"
           >
             <div className="p-4 flex items-center justify-center">
-              <h3 className="text-large-semi">Cart</h3>
+              <h3 className="text-large-semi">Panier</h3>
             </div>
             {cartState && cartState.items?.length ? (
               <>
@@ -160,7 +160,7 @@ const CartDropdown = ({
                                   data-testid="cart-item-quantity"
                                   data-value={item.quantity}
                                 >
-                                  Quantity: {item.quantity}
+                                  Quantité: {item.quantity}
                                 </span>
                               </div>
                               <div className="flex justify-end">
@@ -173,7 +173,7 @@ const CartDropdown = ({
                             className="mt-1"
                             data-testid="cart-item-remove-button"
                           >
-                            Remove
+                            Supprimer
                           </DeleteButton>
                         </div>
                       </div>
@@ -182,8 +182,8 @@ const CartDropdown = ({
                 <div className="p-4 flex flex-col gap-y-4 text-small-regular">
                   <div className="flex items-center justify-between">
                     <span className="text-ui-fg-base font-semibold">
-                      Subtotal{" "}
-                      <span className="font-normal">(excl. taxes)</span>
+                      Sous-total{" "}
+                      <span className="font-normal">(hors taxes)</span>
                     </span>
                     <span
                       className="text-large-semi"
@@ -202,7 +202,7 @@ const CartDropdown = ({
                       size="large"
                       data-testid="go-to-cart-button"
                     >
-                      Go to cart
+                      Aller au panier
                     </Button>
                   </LocalizedClientLink>
                 </div>
@@ -213,12 +213,12 @@ const CartDropdown = ({
                   <div className="bg-gray-900 text-small-regular flex items-center justify-center w-6 h-6 rounded-full text-white">
                     <span>0</span>
                   </div>
-                  <span>Your shopping bag is empty.</span>
+                  <span>Votre panier est vide.</span>
                   <div>
                     <LocalizedClientLink href="/store">
                       <>
-                        <span className="sr-only">Go to all products page</span>
-                        <Button className="flex justify-center items-center text-center px-3 py-4 bg-black font-medium text-sm h-9 text-white border-black border-solid rounded-full hover:bg-white hover:text-black active:bg-white" onClick={close}>Explore products <MoveRightIcon className="ml-2 h-4 w-4" /></Button>
+                        <span className="sr-only">Aller à la page de tous les produits</span>
+                        <Button className="flex justify-center items-center text-center px-3 py-4 bg-black font-medium text-sm h-9 text-white border-black border-solid rounded-full hover:bg-white hover:text-black active:bg-white" onClick={close}>Explorer les produits <MoveRightIcon className="ml-2 h-4 w-4" /></Button>
                       </>
                     </LocalizedClientLink>
                   </div>
