8f004a6d0bea5ab8536c35013047686062abbc2c
anassfiqhi3 <anassfiqhi3@gmail.com>
2025-07-15 00:10:59 +0100

    Modifying the style of login in cart page

--- storefront/src/modules/cart/components/sign-in-prompt/index.tsx
+++ storefront/src/modules/cart/components/sign-in-prompt/index.tsx
@@ -4,7 +4,7 @@ import { MoveRightIcon } from "lucide-react"
 
 const SignInPrompt = () => {
   return (
-    <div className="bg-white flex items-center justify-between">
+    <div className="bg-white flex flex-col md:flex-row items-center justify-between">
       <div>
         <Heading level="h2" className="txt-xlarge">
           Vous avez déjà un compte ?
@@ -15,9 +15,9 @@ const SignInPrompt = () => {
       </div>
       <div>
         <LocalizedClientLink href="/account">
-          <Button className="h-10 flex justify-center items-center text-center px-3 py-4 bg-black font-medium text-sm text-white border-black border-solid rounded-full hover:text-black hover:bg-transparent" data-testid="sign-in-button">
+          <Button className="h-10 flex justify-center items-center text-center px-3 py-4 bg-transparent font-medium text-sm text-black border-black border-solid rounded-full" data-testid="sign-in-button">
             Se connecter
-            <MoveRightIcon className="ml-2 h-4 w-4 hover:text-black" />
+            <MoveRightIcon className="ml-2 h-4 w-4 text-black" />
           </Button>
         </LocalizedClientLink>
       </div>
