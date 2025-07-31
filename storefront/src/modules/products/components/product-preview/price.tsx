5e7b882c988e475d24bdded7b902c1dc471a88a2
anassfiqhi3 <anassfiqhi3@gmail.com>
2025-07-31 21:37:21 +0100

    Changing the button style and PreviewPrice Formatting

--- storefront/src/modules/products/components/product-preview/price.tsx
+++ storefront/src/modules/products/components/product-preview/price.tsx
@@ -1,6 +1,7 @@
 import { Text, clx } from "@medusajs/ui"
 import { VariantPrice } from "types/global"
 import { formatCurrency } from "@lib/util/format-currency"
+import { convertToLocale } from "@/lib/util/money"
 
 export default async function PreviewPrice({ price }: { price: VariantPrice }) {
   if (!price) {
@@ -14,7 +15,11 @@ export default async function PreviewPrice({ price }: { price: VariantPrice }) {
           className="line-through text-ui-fg-muted"
           data-testid="original-price"
         >
-          {formatCurrency(price.original_price_number, price.currency_code)}
+          {/* {formatCurrency(price.original_price_number, price.currency_code)} */}
+          {convertToLocale({
+            amount: price.original_price_number,
+            currency_code: price.currency_code,
+          })}
         </Text>
       )}
       <Text
@@ -23,7 +28,11 @@ export default async function PreviewPrice({ price }: { price: VariantPrice }) {
         })}
         data-testid="price"
       >
-        {formatCurrency(price.calculated_price_number, price.currency_code)}
+        {/* {formatCurrency(price.calculated_price_number, price.currency_code)} */}
+        {convertToLocale({
+          amount: price.calculated_price_number,
+          currency_code: price.currency_code,
+        })}
       </Text>
     </>
   )
