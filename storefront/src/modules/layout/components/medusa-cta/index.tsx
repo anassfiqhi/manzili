7bc7823edc196047af16e3e74c5f954ed3c41c8c
anassfiqhi3 <anassfiqhi3@gmail.com>
2025-08-18 16:41:17 +0100

    Using InteractiveLink and changing to color to blue

--- storefront/src/modules/layout/components/medusa-cta/index.tsx
+++ storefront/src/modules/layout/components/medusa-cta/index.tsx
@@ -1,16 +1,16 @@
 import { Text } from "@medusajs/ui"
 
-import Medusa from "../../../common/icons/medusa"
-import NextJs from "../../../common/icons/nextjs"
-import { ArrowUpRightMini } from "@medusajs/icons"
+// import Medusa from "../../../common/icons/medusa"
+// import NextJs from "../../../common/icons/nextjs"
+import InteractiveLink from "../../../common/components/interactive-link"
 
 const ManziliCTA = () => {
   return (
     <Text className="flex gap-x-2 txt-compact-small-plus items-center">
       Powered by
-      <a href="https://anassfiqhi.dev" target="_blank" rel="noreferrer" className="flex justify-center items-center gap-1 text-blue-600 hover:text-blue-800 transition-colors">
-        anassfiqhi.dev <ArrowUpRightMini className="w-4 h-4" />
-      </a>
+      <InteractiveLink className="text-blue-600 hover:text-blue-800 transition-colors" href="https://anassfiqhi.dev">
+        anassfiqhi.dev
+      </InteractiveLink>
       {/* &
       <a href="https://nextjs.org" target="_blank" rel="noreferrer">
         <NextJs fill="#9ca3af" />
