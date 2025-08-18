2e70de416cff182586aa608245566cc8d65e5d0b
anassfiqhi3 <anassfiqhi3@gmail.com>
2025-08-18 14:14:09 +0100

    changing the cta to anassfiqhi.dev

--- storefront/src/modules/layout/components/medusa-cta/index.tsx
+++ storefront/src/modules/layout/components/medusa-cta/index.tsx
@@ -2,14 +2,14 @@ import { Text } from "@medusajs/ui"
 
 import Medusa from "../../../common/icons/medusa"
 import NextJs from "../../../common/icons/nextjs"
-import { Linkedin } from "@medusajs/icons"
+import { ArrowUpRightMini } from "@medusajs/icons"
 
 const ManziliCTA = () => {
   return (
     <Text className="flex gap-x-2 txt-compact-small-plus items-center">
       Powered by
-      <a href="https://anassfiqhi.dev" target="_blank" rel="noreferrer" className="flex justify-center items-center gap-2">
-        anassfiqhi.dev
+      <a href="https://anassfiqhi.dev" target="_blank" rel="noreferrer" className="flex justify-center items-center gap-1 text-blue-600 hover:text-blue-800 transition-colors">
+        anassfiqhi.dev <ArrowUpRightMini className="w-4 h-4" />
       </a>
       {/* &
       <a href="https://nextjs.org" target="_blank" rel="noreferrer">
